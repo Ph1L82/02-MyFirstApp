beb8c3e43f4e91b40349c6c22f85b22a77405900
edu82 <eduardo@philipsoft.cl>
2024-12-06 18:52:41 -0300

    tarea6

--- src/presentation/screens/HomeWorkScreen.tsx
+++ src/presentation/screens/HomeWorkScreen.tsx
@@ -13,24 +13,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28425B',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'stretch',
     justifyContent: 'space-between',
     paddingVertical: 20,
   },
   box: {
-    width: 100,
+    // width: 100,
     // height: 100,
     borderWidth: 10,
     borderColor: 'white',
   },
   purpleBox: {
     backgroundColor: '#5856D6',
+    flex: 2,
   },
   orangeBox: {
     backgroundColor: '#F0A23B',
+    flex: 2,
   },
   blueBox: {
     backgroundColor: '#28C4D9',
+    flex: 3,
   },
 });
